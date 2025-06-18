7b54f05bc63d984ce4e41ac152080ef5d79458cf
Aseel Odeh <odeh.aseel99@yahoo.com>
2025-06-19 01:38:53 +0300

    saved local files

--- .history/sunduqi-backend/routes/adminRoutes_20250615220724.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-router.get('/dashboard-stats', adminController.getDashboardStats);
-router.get('/dashboard-stats/casher', auth, adminController.getCasherStats);
-
-module.exports = router;
